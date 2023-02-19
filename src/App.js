bcdb0963bcf4936f5598f7fab48dc4e6901c0204
Tiago Costa <tiagodpv@gmail.com>
2023-02-18 23:18:28 -0500

    add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Feed from './components/Feed';
+import Rightbar from './components/Rightbar';
+import Sidebar from './components/Sidebar';
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <box>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </box>
+  );
 }
 
-export default App; ////
+export default App;
